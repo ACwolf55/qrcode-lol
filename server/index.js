8f0a5301f6c4a9419772eb6606901508fee0cb28
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-01-29 19:50:12 -0500

    deploy heroku 7

--- server/index.js
+++ server/index.js
@@ -13,48 +13,48 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static(path.resolve(`${__dirname}/../build`))) 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 app.post('/emailReferal',async (req,res)=>{
-
+    
     const {friendEmail,email} = req.body
     console.log(friendEmail)
     const mailOptions={
-      from:'alexcwolf5@gmail.com',
-      to:friendEmail,
-      subject: 'You Have Been Referred!',
-      text: ` YOU HAVE BEEN REFERRED! ${email} would like to share 1800-Plumber with you!`
+        from:'alexcwolf5@gmail.com',
+        to:friendEmail,
+        subject: 'You Have Been Referred!',
+        text: ` YOU HAVE BEEN REFERRED! ${email} would like to share 1800-Plumber with you!`
     }
     // pass:zjiunqmadmruutxo
     const transport = await nodemailer.createTransport({
-    //   host: 'mail.privateemail.com', 
-    //   port: 587, 
-    //   // tls: { rejectUnauthorized: false},
-    //   // secure: false, 
-    //   debug: true, // show debug output
-    // logger: true,
-    //   auth:{
-    //     user:'referral@overlap.services',
-    //     pass:'sptjolmkzxnadwht'
-    //   }
-    host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // use SSL
-      auth:{
+        //   host: 'mail.privateemail.com', 
+        //   port: 587, 
+        //   // tls: { rejectUnauthorized: false},
+        //   // secure: false, 
+        //   debug: true, // show debug output
+        // logger: true,
+        //   auth:{
+            //     user:'referral@overlap.services',
+            //     pass:'sptjolmkzxnadwht'
+            //   }
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // use SSL
+            auth:{
         user:'1800plumberscottsdale2@gmail.com',
         pass:'bkmcxwsuuxpdynda'
       }
   
     })
     transport.sendMail(mailOptions,(error,info)=>{
-      if(error){
-        console.log(error)
-        return res.status(400).send('error')
-      }else{
-        console.log('email sent') + info.response
+        if(error){
+            console.log(error)
+            return res.status(400).send('error')
+        }else{
+            console.log('email sent') + info.response
         return res.status(200).send('email sent')
-      }
-    })
-  
-  })
+    }
+})
+
+})
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
